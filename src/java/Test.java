2b49b5316eb6741665b07bf6e599f5cb2d42b1f0
‘zhupeng911’ <377964646@qq.com>
2021-06-04 11:47:44 +0800

    branch-master

--- src/java/Test.java
+++ src/java/Test.java
@@ -3,7 +3,7 @@ import cn.hyperchain.User;
 public class Test{
 	public static void main(String[] args) {
 
-		User user = new User("zupeng",23);
+		User user = new User("zhupeng",23);
 		System.out.println("username:"+user.getUserage());
 		System.out.println("userage:"+user.getUserage());
 	}
